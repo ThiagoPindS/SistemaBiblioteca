f89f0c85601208202df0647b49b2ced159e47a8b
Thiago Pinheiro <119745396+ThiagoPindS@users.noreply.github.com>
2025-04-01 21:00:50 -0300

    feat-consultar
    Primeira versão da função de consultar livros

---
+++ Classes/Livro.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prova_POO_Abril_Ian_Pereira.Classes
+{
+    public class Livro
+    {
+        public string Titulo {  get; set; }
+
+        public string Autor {  get; set; }
+
+        public string AnoDaPublicacao { get; set; }
+
+        public string Status {  get; set; }
+    }
+}
