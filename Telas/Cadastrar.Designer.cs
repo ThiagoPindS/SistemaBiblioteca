b1d9bf0ddf1bab2f81c6383b75b0e5a85350dac9
Thiago Pinheiro <119745396+ThiagoPindS@users.noreply.github.com>
2025-04-09 13:12:21 -0300

    Add files via upload

---
+++ Telas/Cadastrar.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace Prova_POO_Abril_Ian_Pereira
+{
+    partial class Cadastrar
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtTitulo = new TextBox();
+            txtAutor = new TextBox();
+            txtAno = new TextBox();
+            cbxStatus = new ComboBox();
+            lblTitulo = new Label();
+            lblAutor = new Label();
+            lblAnoDaPublicacao = new Label();
+            lblStatus = new Label();
+            btnCadastrarLivro = new Button();
+            btnLimparCampos = new Button();
+            btnConsultaDeLivros = new Button();
+            lblTelaDeCadastro = new Label();
+            SuspendLayout();
+            // 
+            // txtTitulo
+            // 
+            txtTitulo.Location = new Point(136, 68);
+            txtTitulo.Name = "txtTitulo";
+            txtTitulo.Size = new Size(168, 23);
+            txtTitulo.TabIndex = 0;
+            // 
+            // txtAutor
+            // 
+            txtAutor.Location = new Point(136, 93);
+            txtAutor.Name = "txtAutor";
+            txtAutor.Size = new Size(168, 23);
+            txtAutor.TabIndex = 1;
+            // 
+            // txtAno
+            // 
+            txtAno.Location = new Point(136, 119);
+            txtAno.Name = "txtAno";
+            txtAno.Size = new Size(168, 23);
+            txtAno.TabIndex = 2;
+            txtAno.KeyPress += txtAno_KeyPress;
+            // 
+            // cbxStatus
+            // 
+            cbxStatus.FormattingEnabled = true;
+            cbxStatus.Items.AddRange(new object[] { "Disponível", "Emprestado" });
+            cbxStatus.Location = new Point(136, 146);
+            cbxStatus.Name = "cbxStatus";
+            cbxStatus.Size = new Size(168, 23);
+            cbxStatus.TabIndex = 3;
+            // 
+            // lblTitulo
+            // 
+            lblTitulo.AutoSize = true;
+            lblTitulo.Location = new Point(10, 74);
+            lblTitulo.Name = "lblTitulo";
+            lblTitulo.Size = new Size(41, 15);
+            lblTitulo.TabIndex = 4;
+            lblTitulo.Text = "Título:";
+            // 
+            // lblAutor
+            // 
+            lblAutor.AutoSize = true;
+            lblAutor.Location = new Point(11, 98);
+            lblAutor.Name = "lblAutor";
+            lblAutor.Size = new Size(40, 15);
+            lblAutor.TabIndex = 5;
+            lblAutor.Text = "Autor:";
+            // 
+            // lblAnoDaPublicacao
+            // 
+            lblAnoDaPublicacao.AutoSize = true;
+            lblAnoDaPublicacao.Location = new Point(11, 124);
+            lblAnoDaPublicacao.Name = "lblAnoDaPublicacao";
+            lblAnoDaPublicacao.Size = new Size(109, 15);
+            lblAnoDaPublicacao.TabIndex = 6;
+            lblAnoDaPublicacao.Text = "Ano da Publicação:";
+            // 
+            // lblStatus
+            // 
+            lblStatus.AutoSize = true;
+            lblStatus.Location = new Point(11, 152);
+            lblStatus.Name = "lblStatus";
+            lblStatus.Size = new Size(42, 15);
+            lblStatus.TabIndex = 7;
+            lblStatus.Text = "Status:";
+            // 
+            // btnCadastrarLivro
+            // 
+            btnCadastrarLivro.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnCadastrarLivro.Location = new Point(10, 182);
+            btnCadastrarLivro.Name = "btnCadastrarLivro";
+            btnCadastrarLivro.Size = new Size(222, 56);
+            btnCadastrarLivro.TabIndex = 8;
+            btnCadastrarLivro.Text = "Cadastrar Livro";
+            btnCadastrarLivro.UseVisualStyleBackColor = true;
+            btnCadastrarLivro.Click += btnCadastrarLivro_Click;
+            // 
+            // btnLimparCampos
+            // 
+            btnLimparCampos.Location = new Point(238, 182);
+            btnLimparCampos.Name = "btnLimparCampos";
+            btnLimparCampos.Size = new Size(65, 56);
+            btnLimparCampos.TabIndex = 9;
+            btnLimparCampos.Text = "Limpar Campos";
+            btnLimparCampos.UseVisualStyleBackColor = true;
+            btnLimparCampos.Click += btnLimparCampos_Click;
+            // 
+            // btnConsultaDeLivros
+            // 
+            btnConsultaDeLivros.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnConsultaDeLivros.Location = new Point(10, 253);
+            btnConsultaDeLivros.Name = "btnConsultaDeLivros";
+            btnConsultaDeLivros.Size = new Size(291, 56);
+            btnConsultaDeLivros.TabIndex = 10;
+            btnConsultaDeLivros.Text = "ConsultaDeLivros";
+            btnConsultaDeLivros.UseVisualStyleBackColor = true;
+            btnConsultaDeLivros.Click += btnConsulta_Click;
+            // 
+            // lblTelaDeCadastro
+            // 
+            lblTelaDeCadastro.AutoSize = true;
+            lblTelaDeCadastro.Font = new Font("Segoe UI", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblTelaDeCadastro.Location = new Point(21, 9);
+            lblTelaDeCadastro.Name = "lblTelaDeCadastro";
+            lblTelaDeCadastro.Size = new Size(267, 45);
+            lblTelaDeCadastro.TabIndex = 11;
+            lblTelaDeCadastro.Text = "Tela de Cadastro";
+            // 
+            // Cadastrar
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(315, 318);
+            Controls.Add(lblTelaDeCadastro);
+            Controls.Add(btnConsultaDeLivros);
+            Controls.Add(btnLimparCampos);
+            Controls.Add(btnCadastrarLivro);
+            Controls.Add(lblStatus);
+            Controls.Add(lblAnoDaPublicacao);
+            Controls.Add(lblAutor);
+            Controls.Add(lblTitulo);
+            Controls.Add(cbxStatus);
+            Controls.Add(txtAno);
+            Controls.Add(txtAutor);
+            Controls.Add(txtTitulo);
+            Name = "Cadastrar";
+            Text = "BiblioSystem";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtTitulo;
+        private TextBox txtAutor;
+        private TextBox txtAno;
+        private ComboBox cbxStatus;
+        private Label lblTitulo;
+        private Label lblAutor;
+        private Label lblAnoDaPublicacao;
+        private Label lblStatus;
+        private Button btnCadastrarLivro;
+        private Button btnLimparCampos;
+        private Button btnConsultaDeLivros;
+        private Label lblTelaDeCadastro;
+    }
+}
---
+++ Telas/Consulta.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace Prova_POO_Abril_Ian_Pereira
+{
+    partial class Consulta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblTelaDeConsulta = new Label();
+            dgvAcervo = new DataGridView();
+            txtPesquisar = new TextBox();
+            btnRemover = new Button();
+            btnEditar = new Button();
+            btnAlterarStatus = new Button();
+            btnCadastro = new Button();
+            lblPesquisar = new Label();
+            lblStatus = new Label();
+            cbxStatus = new ComboBox();
+            lblTitulo = new Label();
+            txtTitulo = new TextBox();
+            lblAutor = new Label();
+            txtAutor = new TextBox();
+            lblAnoDaPublicacao = new Label();
+            txtAnoDaPublicacao = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)dgvAcervo).BeginInit();
+            SuspendLayout();
+            // 
+            // lblTelaDeConsulta
+            // 
+            lblTelaDeConsulta.AutoSize = true;
+            lblTelaDeConsulta.Font = new Font("Segoe UI", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblTelaDeConsulta.Location = new Point(93, 9);
+            lblTelaDeConsulta.Name = "lblTelaDeConsulta";
+            lblTelaDeConsulta.Size = new Size(264, 45);
+            lblTelaDeConsulta.TabIndex = 12;
+            lblTelaDeConsulta.Text = "Tela de Consulta";
+            // 
+            // dgvAcervo
+            // 
+            dgvAcervo.AllowUserToAddRows = false;
+            dgvAcervo.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvAcervo.EditMode = DataGridViewEditMode.EditProgrammatically;
+            dgvAcervo.Location = new Point(12, 86);
+            dgvAcervo.Name = "dgvAcervo";
+            dgvAcervo.ReadOnly = true;
+            dgvAcervo.RowHeadersVisible = false;
+            dgvAcervo.RowHeadersWidth = 51;
+            dgvAcervo.SelectionMode = DataGridViewSelectionMode.CellSelect;
+            dgvAcervo.Size = new Size(442, 266);
+            dgvAcervo.TabIndex = 13;
+            dgvAcervo.CellClick += dgvAcervo_CellClick;
+            // 
+            // txtPesquisar
+            // 
+            txtPesquisar.Location = new Point(78, 57);
+            txtPesquisar.Name = "txtPesquisar";
+            txtPesquisar.Size = new Size(201, 23);
+            txtPesquisar.TabIndex = 14;
+            txtPesquisar.TextChanged += txtPesquisar_TextChanged;
+            // 
+            // btnRemover
+            // 
+            btnRemover.Location = new Point(299, 416);
+            btnRemover.Name = "btnRemover";
+            btnRemover.Size = new Size(75, 23);
+            btnRemover.TabIndex = 19;
+            btnRemover.Text = "Remover Livro";
+            btnRemover.UseVisualStyleBackColor = true;
+            btnRemover.Click += btnRemover_Click;
+            // 
+            // btnEditar
+            // 
+            btnEditar.Location = new Point(379, 416);
+            btnEditar.Name = "btnEditar";
+            btnEditar.Size = new Size(75, 23);
+            btnEditar.TabIndex = 20;
+            btnEditar.Text = "Editar Livro";
+            btnEditar.UseVisualStyleBackColor = true;
+            btnEditar.Click += btnEditar_Click;
+            // 
+            // btnAlterarStatus
+            // 
+            btnAlterarStatus.Location = new Point(206, 416);
+            btnAlterarStatus.Name = "btnAlterarStatus";
+            btnAlterarStatus.Size = new Size(87, 23);
+            btnAlterarStatus.TabIndex = 21;
+            btnAlterarStatus.Text = "Alterar Status";
+            btnAlterarStatus.UseVisualStyleBackColor = true;
+            btnAlterarStatus.Click += btnAlterarStatus_Click;
+            // 
+            // btnCadastro
+            // 
+            btnCadastro.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnCadastro.Location = new Point(12, 445);
+            btnCadastro.Name = "btnCadastro";
+            btnCadastro.Size = new Size(442, 68);
+            btnCadastro.TabIndex = 22;
+            btnCadastro.Text = "Voltar ao Cadastro";
+            btnCadastro.UseVisualStyleBackColor = true;
+            btnCadastro.Click += btnCadastro_Click;
+            // 
+            // lblPesquisar
+            // 
+            lblPesquisar.AutoSize = true;
+            lblPesquisar.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblPesquisar.Location = new Point(12, 65);
+            lblPesquisar.Name = "lblPesquisar";
+            lblPesquisar.Size = new Size(60, 15);
+            lblPesquisar.TabIndex = 23;
+            lblPesquisar.Text = "Pesquisar:";
+            // 
+            // lblStatus
+            // 
+            lblStatus.AutoSize = true;
+            lblStatus.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblStatus.Location = new Point(285, 65);
+            lblStatus.Name = "lblStatus";
+            lblStatus.Size = new Size(42, 15);
+            lblStatus.TabIndex = 24;
+            lblStatus.Text = "Status:";
+            // 
+            // cbxStatus
+            // 
+            cbxStatus.FormattingEnabled = true;
+            cbxStatus.Items.AddRange(new object[] { "Todos", "Disponível", "Emprestado" });
+            cbxStatus.Location = new Point(333, 57);
+            cbxStatus.Name = "cbxStatus";
+            cbxStatus.Size = new Size(121, 23);
+            cbxStatus.TabIndex = 25;
+            cbxStatus.Text = "Todos";
+            cbxStatus.SelectedIndexChanged += cbxStatus_SelectedIndexChanged;
+            // 
+            // lblTitulo
+            // 
+            lblTitulo.AutoSize = true;
+            lblTitulo.Location = new Point(12, 366);
+            lblTitulo.Name = "lblTitulo";
+            lblTitulo.Size = new Size(41, 15);
+            lblTitulo.TabIndex = 28;
+            lblTitulo.Text = "Título:";
+            // 
+            // txtTitulo
+            // 
+            txtTitulo.Location = new Point(59, 358);
+            txtTitulo.Name = "txtTitulo";
+            txtTitulo.Size = new Size(74, 23);
+            txtTitulo.TabIndex = 29;
+            // 
+            // lblAutor
+            // 
+            lblAutor.AutoSize = true;
+            lblAutor.Location = new Point(139, 366);
+            lblAutor.Name = "lblAutor";
+            lblAutor.Size = new Size(40, 15);
+            lblAutor.TabIndex = 30;
+            lblAutor.Text = "Autor:";
+            // 
+            // txtAutor
+            // 
+            txtAutor.Location = new Point(185, 358);
+            txtAutor.Name = "txtAutor";
+            txtAutor.Size = new Size(74, 23);
+            txtAutor.TabIndex = 34;
+            // 
+            // lblAnoDaPublicacao
+            // 
+            lblAnoDaPublicacao.AutoSize = true;
+            lblAnoDaPublicacao.Location = new Point(265, 366);
+            lblAnoDaPublicacao.Name = "lblAnoDaPublicacao";
+            lblAnoDaPublicacao.Size = new Size(109, 15);
+            lblAnoDaPublicacao.TabIndex = 35;
+            lblAnoDaPublicacao.Text = "Ano da Publicação:";
+            // 
+            // txtAnoDaPublicacao
+            // 
+            txtAnoDaPublicacao.Location = new Point(379, 358);
+            txtAnoDaPublicacao.Name = "txtAnoDaPublicacao";
+            txtAnoDaPublicacao.Size = new Size(74, 23);
+            txtAnoDaPublicacao.TabIndex = 36;
+            // 
+            // Consulta
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(466, 523);
+            Controls.Add(txtAnoDaPublicacao);
+            Controls.Add(lblAnoDaPublicacao);
+            Controls.Add(txtAutor);
+            Controls.Add(lblAutor);
+            Controls.Add(txtTitulo);
+            Controls.Add(lblTitulo);
+            Controls.Add(cbxStatus);
+            Controls.Add(lblStatus);
+            Controls.Add(lblPesquisar);
+            Controls.Add(btnCadastro);
+            Controls.Add(btnAlterarStatus);
+            Controls.Add(btnEditar);
+            Controls.Add(btnRemover);
+            Controls.Add(txtPesquisar);
+            Controls.Add(dgvAcervo);
+            Controls.Add(lblTelaDeConsulta);
+            Name = "Consulta";
+            Text = "Consulta";
+            ((System.ComponentModel.ISupportInitialize)dgvAcervo).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblTelaDeConsulta;
+        public DataGridView dgvAcervo;
+        public TextBox txtPesquisar;
+        private Button btnRemover;
+        private Button btnEditar;
+        private Button btnAlterarStatus;
+        private Button btnCadastro;
+        private Label lblPesquisar;
+        private Label lblStatus;
+        private ComboBox cbxStatus;
+        private Label lblTitulo;
+        private TextBox txtTitulo;
+        private Label lblAutor;
+        private TextBox txtAutor;
+        private Label lblAnoDaPublicacao;
+        private TextBox txtAnoDaPublicacao;
+    }
+}
